309dfdf62288f834c91648e5ca54e82e42e4663c
Gary McGhee <gary@buzzware.com.au>
2019-04-17 16:04:18 +0800

    added docs

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const {FUNCTION_NAME} = process.env;
+const ON_GCP = !!process.env.X_GOOGLE_FUNCTION_REGION;
+const ON_EMULATOR = !ON_GCP;
+
 const functions = require('firebase-functions');
 
 const feathers = require('@feathersjs/feathers');
@@ -24,11 +27,27 @@ mainApp.all('*', async (req, res, next) => {
   return next();
 });
 
-let app,api;
+/*
+
+Whats going on here ?
+
+Background :
+* GCP creates a separate runtime per function, and each function
+executes with FUNCTION_NAME set to the name of the assigned function.
+* There is no need to initialise dependencies for functions that the
+current runtime will never serve. So we can save memory and startup time
+by conditionally setting each one up.
+* We must appear to export all functions we ever serve here, for firebase functions
+to find and allocate runtimes. In reality, we only export a blank express app for
+functions we will never execute in each runtime.
+* There are undocumented rules for how Firebase discovers exported functions, and
+some methods of conditional exports caused them to fail to be found, so this is code
+is a bit fragile.
+*/
 
 console.log('setting up '+FUNCTION_NAME);
 if (FUNCTION_NAME=='app') {
-  app = require('./app/index').app;
+  const app = require('./app/index').app;
   app(mainApp);
 } else if (FUNCTION_NAME=='api') {
   const MyService = require('./services/MyService');
@@ -40,9 +59,6 @@ if (FUNCTION_NAME=='app') {
   api.setup(mainApp);  // required by feathers for subapps
 }
 
-// // do this later to separate hosting and save startup time and memory
-// exports.app = app ? functions.https.onRequest(mainApp) : dummy();
-// exports.api = api ? functions.https.onRequest(mainApp) : dummy();
-
 exports.app = functions.https.onRequest(mainApp);
 exports.api = functions.https.onRequest(mainApp);
+
